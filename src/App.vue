0ceaaca8e81d1f6710802442c14fb92c00541248
Vlad <vladgrichan@gmail.com>
2021-02-02 21:20:06 +0000

    added todo list completed/not

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Todos :todos="todos" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todos
+  },
+  // the state/data
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "todo 3",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="todo-item"
+    v-bind:class="{ 'is-complete': mutableTodo.completed }"
+  >
+    <p>
+      <input type="checkbox" v-on:change="markComplete()" />
+      {{ todo.title }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.mutableTodo.completed = !this.mutableTodo.completed;
+    }
+  },
+  data: function() {
+    return {
+      mutableTodo: this.todo
+    };
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f3f3f3;
+  padding: 1em;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+</style>
