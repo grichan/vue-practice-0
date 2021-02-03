6c50ab5080f0aa9a985ca895ede94808ad616b98
Vlad <vladgrichan@gmail.com>
2021-02-03 22:01:47 +0000

    added the add todo button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
+    <Header />
+    <!-- v-on is catching sent data -->
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos :todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: "App",
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   // the state/data
   data() {
@@ -37,6 +44,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
