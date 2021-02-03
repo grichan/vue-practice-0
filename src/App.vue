579ffdb0a3bfeef8ec997b41d504e378a6baa1e5
Vlad <vladgrichan@gmail.com>
2021-02-03 22:03:51 +0000

    added axious

--- src/App.vue
+++ src/App.vue
@@ -22,23 +22,7 @@ export default {
   // the state/data
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "todo 1",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "todo 2",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "todo 3",
-          completed: false
-        }
-      ]
+      todos: []
     };
   },
   methods: {
